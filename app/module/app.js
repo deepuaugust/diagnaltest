73defbdd8dd495a3970cb832022f27a68e610a51
deepuaugust <32448744+deepuaugust@users.noreply.github.com>
2018-05-25 12:06:27 +0530

    diagnal first version
    first committ

---
+++ app/module/app.js
@@ -0,0 +1,38 @@
+var myApp = null;
+(function () {
+    myApp = angular.module("app", ['infinite-scroll'])
+
+
+        .controller("videoController", ["$scope", "$http", function ($scope, $http) {
+            console.log("in videoController");
+            var array = [];
+
+            $http.get("../app/model/CONTENTLISTINGPAGE-PAGE1.json")
+                .then(function (response) {
+                    $scope.videoData = response.data;
+                    array.push(response.data.page['content-items'].content);
+                    //$scope.tiles.push();
+                });
+
+                $http.get("../app/model/CONTENTLISTINGPAGE-PAGE2.json")
+                .then(function (response) {
+                    $scope.videoData = response.data;
+                   array.push(response.data.page['content-items'].content);
+                });
+
+                $http.get("../app/model/CONTENTLISTINGPAGE-PAGE2.json")
+                .then(function (response) {
+                    $scope.videoData = response.data;
+                    array.push(response.data.page['content-items'].content);
+                    $scope.tiles = array;
+                });
+
+            $scope.loadMore = function () {
+                var last = $scope.tiles[$scope.tiles.length - 1];
+                for (var i = 1; i <= 5; i++) {
+                    $scope.tiles.push(last + i);
+                }
+            };
+        }])
+
+}());
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".searchbox").css('visibility', 'visible');
+    // $(".search").click(function () {
+    //     if ($(".searchbox").is(":visible") == true)
+    //         $(".searchbox").css('visibility', 'hidden');
+    //     else
+    //         $(".searchbox").css('visibility', 'visible');
+    // });
+})
