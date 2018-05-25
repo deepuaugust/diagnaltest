6ed1732cc9621a3e62209446f7b0b6f6c5546172
deepuaugust <32448744+deepuaugust@users.noreply.github.com>
2018-05-25 16:02:04 +0530

    revert scroll

--- app/module/app.js
+++ app/module/app.js
@@ -6,7 +6,6 @@ var myApp = null;
         .controller("videoController", ["$scope", "$http", function ($scope, $http) {
             console.log("in videoController");
             var array = [];
-            $scope.numberToDisplay = 10;
 
             $http.get("../app/model/CONTENTLISTINGPAGE-PAGE1.json")
                 .then(function (response) {
@@ -27,23 +26,14 @@ var myApp = null;
                     array.push(response.data.page['content-items'].content);
                     $scope.tiles = array;
                     $scope.counter = array[0].length + array[1].length + array[2].length;
-                    // for (var i = 0; i < array[0].length; i++) {
-                    //     $scope.allItems.push(array[0][i]);
-                    // }
-                    // for (var i = 0; i < array[1].length; i++) {
-                    //     $scope.allItems.push(array[1][i]);
-                    // }
-                    // for (var i = 0; i < array[2].length; i++) {
-                    //     $scope.allItems.push(array[2][i]);
-                    // }
                 });
 
-            $scope.loadMore = function () {
-                var last = $scope.tiles[$scope.counter - 1];
-                for (var i = 1; i <= 8; i++) {
-                    $scope.tiles.push(last + i);
-                }
-            };
+            // $scope.loadMore = function () {
+            //     var last = $scope.tiles[$scope.counter - 1];
+            //     for (var i = 1; i <= 8; i++) {
+            //         $scope.tiles.push(last + i);
+            //     }
+            // };
         }])
 
 }());
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    $(".searchbox").css('visibility', 'visible');
-    // $(".search").click(function () {
-    //     if ($(".searchbox").is(":visible") == true)
-    //         $(".searchbox").css('visibility', 'hidden');
-    //     else
-    //         $(".searchbox").css('visibility', 'visible');
-    // });
     var submitIcon = $('.searchbox-icon');
     var inputBox = $('.searchbox-input');
     var searchBox = $('.searchbox');
