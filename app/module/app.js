69944ef1af358801820c6128d24917e2916f7082
deepuaugust <32448744+deepuaugust@users.noreply.github.com>
2018-05-25 15:51:55 +0530

    infinite scroll changes

--- app/module/app.js
+++ app/module/app.js
@@ -6,6 +6,7 @@ var myApp = null;
         .controller("videoController", ["$scope", "$http", function ($scope, $http) {
             console.log("in videoController");
             var array = [];
+            $scope.numberToDisplay = 10;
 
             $http.get("../app/model/CONTENTLISTINGPAGE-PAGE1.json")
                 .then(function (response) {
@@ -14,22 +15,32 @@ var myApp = null;
                     //$scope.tiles.push();
                 });
 
-                $http.get("../app/model/CONTENTLISTINGPAGE-PAGE2.json")
+            $http.get("../app/model/CONTENTLISTINGPAGE-PAGE2.json")
                 .then(function (response) {
                     $scope.videoData = response.data;
-                   array.push(response.data.page['content-items'].content);
+                    array.push(response.data.page['content-items'].content);
                 });
 
-                $http.get("../app/model/CONTENTLISTINGPAGE-PAGE2.json")
+            $http.get("../app/model/CONTENTLISTINGPAGE-PAGE2.json")
                 .then(function (response) {
                     $scope.videoData = response.data;
                     array.push(response.data.page['content-items'].content);
                     $scope.tiles = array;
+                    $scope.counter = array[0].length + array[1].length + array[2].length;
+                    // for (var i = 0; i < array[0].length; i++) {
+                    //     $scope.allItems.push(array[0][i]);
+                    // }
+                    // for (var i = 0; i < array[1].length; i++) {
+                    //     $scope.allItems.push(array[1][i]);
+                    // }
+                    // for (var i = 0; i < array[2].length; i++) {
+                    //     $scope.allItems.push(array[2][i]);
+                    // }
                 });
 
             $scope.loadMore = function () {
-                var last = $scope.tiles[$scope.tiles.length - 1];
-                for (var i = 1; i <= 5; i++) {
+                var last = $scope.tiles[$scope.counter - 1];
+                for (var i = 1; i <= 8; i++) {
                     $scope.tiles.push(last + i);
                 }
             };
